1f8cc836a12643e48c2ab97c064c550d2ea64477
Joshua Ryan <shua2727@gmail.com>
2017-11-15 00:35:06 -0500

    Use find instead of bracket access operator

--- skeletons/cpp/src/main.cpp
+++ skeletons/cpp/src/main.cpp
@@ -8,9 +8,9 @@ int main()
     auto response = cpr::Get(cpr::Url{"http://127.0.0.1:5000/CreateChallenge/1"});
     auto responseAsJson(nlohmann::json::parse(response.text));
 
-    auto args(responseAsJson["Argeasdasds"]);
+    auto args(responseAsJson.find("Args"));
 
     std::cout << responseAsJson.dump(4);
-    std::cout << responseAsJson.value("Argsasdasd", "none");
+
     return 0;
 }
