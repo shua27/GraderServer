8471510265bc94044c88d5826b1cb6a3e5bea388
Joshua Ryan <shua2727@gmail.com>
2018-01-23 23:19:23 -0500

    Remove dead code

--- skeletons/java/src/game/client/Main.java
+++ skeletons/java/src/game/client/Main.java
@@ -1,28 +1,8 @@
 package game.client;
 
-import java.io.*;
-import java.net.*;
-
 public class Main {
 
-    public static String getHTML(String urlToRead) throws Exception {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-        String line;
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-        rd.close();
-
-        return result.toString();
-    }
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Mastermind game = new Mastermind();
     }
 }
